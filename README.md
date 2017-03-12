9a3c87e69535026cb4943be69a9aa8f24dbfb699
azu <azuciao@gmail.com>
2017-03-12 18:37:46 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Twitter Bot
+
+Bot program for [@jser_info](https://twitter.com/jser_info "@jser_info").
+
+## Installation
+
+    npm install
+
+## Usage
+
+    npm start
+
+## Commands
+
+### `@jser /(stat|status|ステータス)/`
+
+https://jser.info/status-of-post の結果を返します。
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,53 @@
+/* Setting things up. */
+const path = require('path');
+const statusOfPost = require("./lib/status-of-post");
+const Twit = require('twit');
+const config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/make-an-image-posting-twitter-bot/#creating-a-twitter-app*/
+    twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    }
+};
+const T = new Twit(config.twitter);
+const stream = T.stream('statuses/filter', { track: '@jser_info' });
+const isReply = (tweet) => {
+    return /@jser_info/.test(tweet.text);
+};
+const isStat = (tweet) => {
+    return /(stat|status|ステータス)/.test(tweet.text);
+};
+stream.on('tweet', function(tweet) {
+    if (!isReply(tweet)) {
+        return;
+    }
+    const replyTo = `@${tweet.user.screen_name}`;
+    if (isStat(tweet)) {
+        return statusOfPost().then(text => {
+            T.post('statuses/update', {
+                status: `${replyTo} ${text}`,
+                in_reply_to_status_id: tweet.id
+            }, function(error, data, response) {
+                if (error) {
+                    console.log(error.message, error.stack);
+                }
+            });
+        }).catch(error => {
+            console.log(error.message, error.stack);
+        });
+    }
+});
+
+
+/**********************************************************************************************/
+/* The code below takes care of serving the index.html file, no need to change anything here. */
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
+
